04ab9cb9be3dd38bb589a55efda1a2a23c1f05d4
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-05-29 13:08:09 +0300

    fixed name contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getFilteredContacts } from 'redux/contacts/selectors';
-import {ContactListItem} from '../ContactListItem/ContactListItem';
+import { ContactListItem } from '../ContactListItem/ContactListItem';
 import { Text } from './ContactList.styles';
 
 export const ContactList = () => {
@@ -10,7 +10,7 @@ export const ContactList = () => {
   return (
     <>
       <ul>
-        <Text variant="h4">Contacts</Text>
+        {filteredContacts.length > 0 && <Text variant="h4">Contacts</Text>}
         {filteredContacts.map(contact => (
           <ContactListItem key={contact.id} contact={contact}></ContactListItem>
         ))}
@@ -18,4 +18,3 @@ export const ContactList = () => {
     </>
   );
 };
-
